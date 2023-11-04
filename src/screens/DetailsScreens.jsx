d1ed45a28f3ce30e048a02f169620e3cb0c74d4a
maurice <maurice.arida66@gmail.com>
2023-11-04 21:54:51 +0200

    workign version

---
+++ src/screens/DetailsScreens.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const DetailsScreens = () => {
+  return (
+    <View>
+      <Text>DetailsScreens</Text>
+    </View>
+  );
+};
+
+export default DetailsScreens;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/HomeScreens.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreens = () => {
+  return (
+    <View>
+      <Text>HomeScreens</Text>
+    </View>
+  );
+};
+
+export default HomeScreens;
+
+const styles = StyleSheet.create({});
